a9e144d5de08d85d76e1c4730b0c535c13c34472
Krzysztof Brzozowski <krzysztofbrzozowski7@gmail.com>
2022-09-05 09:27:03 +0100

    added sorting

--- src/components/MainSection/MainSection.jsx
+++ src/components/MainSection/MainSection.jsx
@@ -1,5 +1,4 @@
 import { useState, useContext, useEffect } from "react";
-import Axios from "axios";
 import SearchBar from "../SearchBar/SearchBar";
 import {
   MainSectionContainer,
@@ -9,15 +8,77 @@ import {
 import { DarkModeContext } from "../../App/App";
 import CountryComponent from "../CountryComponent/CountryComponent";
 import { filters } from "../../misc/filters";
+import { sortByList, sortBySwitchList } from "../../misc/sortByList";
 
 export default function MainSection() {
   const darkMode = useContext(DarkModeContext);
   const [searchedBy, setSearchedBy] = useState("name");
+  const [sortBy, setSortBy] = useState("");
   const [searchedCountry, setSearchedCountry] = useState("");
   const [countriesArr, setCountriesArr] = useState([]);
 
-  const handleChange = (e) => {
+  const sortCountries = () => {
+    switch (sortBy) {
+      case sortBySwitchList.Alphabetically:
+        setCountriesArr((prev) =>
+          [...prev].sort((a, b) =>
+            a.name.common < b.name.common
+              ? -1
+              : a.name.common > b.name.common
+              ? 1
+              : 0
+          )
+        );
+        break;
+      case sortBySwitchList.AlphabeticallyReversed:
+        setCountriesArr((prev) =>
+          [...prev].sort((a, b) =>
+            a.name.common < b.name.common
+              ? 1
+              : a.name.common > b.name.common
+              ? -1
+              : 0
+          )
+        );
+        break;
+      case sortBySwitchList.ByPopulationDecreasing:
+        setCountriesArr((prev) =>
+          [...prev].sort((a, b) =>
+            a.population < b.population
+              ? 1
+              : a.population > b.population
+              ? -1
+              : 0
+          )
+        );
+        break;
+      case sortBySwitchList.ByPopulationIncreasing:
+        setCountriesArr((prev) =>
+          [...prev].sort((a, b) =>
+            a.population < b.population
+              ? -1
+              : a.population > b.population
+              ? 1
+              : 0
+          )
+        );
+        break;
+      case sortBySwitchList.ByRegions:
+        setCountriesArr((prev) =>
+          [...prev].sort((a, b) =>
+            a.region < b.region ? -1 : a.region > b.region ? 1 : 0
+          )
+        );
+        break;
+      default:
+        return;
+    }
+  };
+  const changeSearchBy = (e) => {
     setSearchedBy(e.target.value);
+  };
+  const changeSortBy = (e) => {
+    setSortBy(e.target.value);
   };
   const fetchWelcomeCountries = async () => {
     const response = await fetch(`https://restcountries.com/v3.1/all`);
@@ -38,11 +99,15 @@ export default function MainSection() {
   useEffect(() => {
     fetchWelcomeCountries();
   }, []);
+
   useEffect(() => {
     if (searchedCountry) {
       fetchData();
     }
   }, [searchedCountry]);
+  useEffect(() => {
+    sortCountries();
+  }, [sortBy]);
 
   return (
     <MainSectionContainer dark={darkMode}>
@@ -52,7 +117,7 @@ export default function MainSection() {
       />
       <SearchByContainer dark={darkMode}>
         <p>Search by:</p>
-        <select onChange={(e) => handleChange(e)}>
+        <select onChange={(e) => changeSearchBy(e)}>
           {filters.map((item, index) => {
             return (
               <option key={index} value={item.value}>
@@ -61,9 +126,15 @@ export default function MainSection() {
             );
           })}
         </select>
+        <p>Sort:</p>
+        <select onChange={(e) => changeSortBy(e)}>
+          {sortByList.map((item, index) => {
+            return <option key={index}>{item.name}</option>;
+          })}
+        </select>
       </SearchByContainer>
       <FoundCountriesContainer>
-        {countriesArr?.map((item) => {
+        {countriesArr.map((item) => {
           return (
             <CountryComponent
               key={item.name.common}
---
+++ src/misc/sortByList.js
@@ -0,0 +1,25 @@
+const sortByList = [
+  {
+    name: "Alphabetically",
+  },
+  {
+    name: "Alphabetically (reversed)",
+  },
+  {
+    name: "By population (increasing)",
+  },
+  {
+    name: "By population (decreasing)",
+  },
+  {
+    name: "By regions",
+  },
+];
+const sortBySwitchList = {
+  Alphabetically: "Alphabetically",
+  AlphabeticallyReversed: "Alphabetically (reversed)",
+  ByPopulationIncreasing: "By population (increasing)",
+  ByPopulationDecreasing: "By population (decreasing)",
+  ByRegions: "By regions",
+};
+export { sortByList, sortBySwitchList };
