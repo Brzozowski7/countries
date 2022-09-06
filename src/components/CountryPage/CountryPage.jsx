aa6b55cc0b9b5d9e3c3acbb364db8e09048a7c94
Krzysztof Brzozowski <krzysztofbrzozowski7@gmail.com>
2022-09-06 05:12:50 +0100

    basic CountryPage styling

--- src/components/CountryPage/CountryPage.jsx
+++ src/components/CountryPage/CountryPage.jsx
@@ -1,5 +1,85 @@
+import { useState, useEffect, useContext } from "react";
+import { useParams } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
+import {
+  CountryPageWrapper,
+  StyledLink,
+  CountryInformationContainer,
+} from "./CountryPage.styles";
+import { addCommas } from "./CountryPage.utils";
+import { DarkModeContext } from "../../App/App";
 export default function CountryPage() {
+  const darkMode = useContext(DarkModeContext);
+  const [country, setCountry] = useState();
+  const params = useParams();
+  const checkLocalStorage = () => {
+    const localStorageItem = localStorage.getItem(`${params.countryName}`);
+    if (localStorageItem) {
+      setCountry(JSON.parse(localStorageItem));
+      return true;
+    }
+    return false;
+  };
+  const fetchSpecificCountry = async () => {
+    const response = await fetch(
+      `https://restcountries.com/v3.1/name/${params.countryName}?fullText=true`
+    );
+    if (response.ok) {
+      const data = await response.json();
+      setCountry(data);
+      localStorage.setItem(`${params.countryName}`, JSON.stringify(data));
+    }
+  };
+  useEffect(() => {
+    if (!checkLocalStorage()) {
+      fetchSpecificCountry();
+    }
+  }, []);
   return (
-    <div>CountryPage</div>
-  )
+    <CountryPageWrapper dark={darkMode}>
+      <StyledLink dark={darkMode} to="/">
+        <FontAwesomeIcon icon={faArrowLeft} />
+        Back
+      </StyledLink>
+      {country?.map((item) => {
+        return (
+          <>
+            <img src={item.flags.png} alt={`${item.name} flag`} />
+            <CountryInformationContainer>
+              <h2>{item.name.common}</h2>
+              <div>
+                <p>
+                  <b>Native Name:</b> {item.name.official}
+                </p>
+                <p>
+                  <b>Population:</b> {addCommas(item.population)}
+                </p>
+                <p>
+                  <b>Region:</b> {item.region}
+                </p>
+                <p>
+                  <b>Sub Region:</b> {item.subregion}
+                </p>
+                <p>
+                  <b>Capital: </b> {item.capital}
+                </p>
+              </div>
+              <div>
+                <p>
+                  <b>Top Level Domain: </b> {item.tld}
+                </p>
+                <p>
+                  <b>Currencies:</b> {Object.keys(item.currencies).join(", ")}
+                </p>
+                <p>
+                  <b>Languages:</b> {Object.values(item.languages).join(", ")}
+                </p>
+              </div>
+            </CountryInformationContainer>
+          </>
+        );
+      })}
+    </CountryPageWrapper>
+  );
 }
---
+++ src/components/CountryPage/CountryPage.styles.js
@@ -0,0 +1,43 @@
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import { pallete } from "../../misc/pallete";
+
+export const CountryPageWrapper = styled.main`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: left;
+  width: 100%;
+  padding: 2rem 5rem;
+  background-color: ${(props) =>
+    props.dark ? pallete.VeryDarkBlue : pallete.VeryLightGray};
+  @media screen and (max-width: 960px) {
+    gap: 3rem;
+    padding: 2rem;
+  }
+`;
+
+export const StyledLink = styled(Link)`
+  width: 8rem;
+  background-color: ${(props) =>
+    props.dark ? pallete.DarkBlue : pallete.White};
+  color: ${(props) => (props.dark ? pallete.White : pallete.DarkBlue)};
+  font-weight: 800;
+  padding: 0.5rem 2rem;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 0.5rem;
+  text-decoration: none;
+  box-shadow: ${(props) =>
+    props.dark
+      ? `${pallete.DarkGray} 0px 3px 3px`
+      : `${pallete.VeryDarkBlue} 0px 3px 3px`};
+`;
+
+export const CountryInformationContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 600px;
+`;
